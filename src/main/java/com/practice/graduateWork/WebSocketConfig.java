6c1d6039f50b4c05b7086877db7500a71da63829
jakdalmak7961 <jakdalmak7961@gmail.com>
2024-03-20 00:09:32 +0900

    PostgreSQL 연동 설정과 WebSocket 기초 구동 방식 확인

---
+++ src/main/java/com/practice/graduateWork/WebSocketConfig.java
@@ -0,0 +1,47 @@
+package com.practice.graduateWork;
+
+import com.practice.graduateWork.handler.ReplyEchoHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    /** 싱글톤 replyEchoHandler 제공 */
+    @Bean
+    public ReplyEchoHandler replyEchoHandler() {
+        return new ReplyEchoHandler();
+    }
+
+    /** /replyEcho 경로로 들어오는 요청에 대한 처리 핸들러를 replyEchoHandler로 지정하고,
+     * 서버와 연결한 클라이언트의 각 세션의 구분을 위해 HTTP Session 속성을 WebSocket Session에 올려줌
+     * => HttpSessionHandshakeInterceptor의 역할 */
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(replyEchoHandler(), "/replyEcho")
+                .addInterceptors(new HttpSessionHandshakeInterceptor());
+
+    }
+}
+
+/*
+servlet-context.xml
+
+<!-- websocket handler -->
+<beans:bean id="replyEchoHandler" class="com.jade.swp.handler.ReplyEchoHandler" />
+
+<websocket:handlers>
+<websocket:mapping handler="replyEchoHandler" path="/replyEcho" />
+<websocket:handshake-interceptors>
+    <beans:bean
+        class="org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor" />
+</websocket:handshake-interceptors>
+</websocket:handler>
+</beans:beans>
+
+ */
---
+++ src/main/java/com/practice/graduateWork/handler/ReplyEchoHandler.java
@@ -0,0 +1,48 @@
+package com.practice.graduateWork.handler;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 이미지, 영상 등의 실시간 공유를 원한다면 BinaryWebSockethandler를 사용하기
+public class ReplyEchoHandler extends TextWebSocketHandler {
+    List<WebSocketSession> sessions = new ArrayList<>();
+    /** Connection이 연결된 경우 */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        System.out.println("afterConnectionEstablished: " + session);
+
+        sessions.add(session); // 접속한 사용자의 session을 List에 추가하여 다룰 수 있음
+    }
+
+
+    /** 클라이언트가 Socket을 통해 message 보낸 경우 */
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        System.out.println("handleTextmessage:" + session + " : " + message);
+        String senderId = session.getId(); // 모든 HttpSession, WebSocketSession은 id를 반드시 가지게 되어있다
+        
+        for(WebSocketSession sess: sessions) {
+            sess.sendMessage(new TextMessage(senderId + ": " + message.getPayload()));
+            // WebSocket에서 전송할 메시지는 단순한 String이 아닌, TextMessage라는 객체로 다루어짐
+            // 이때, 클라이언트로부터 받아온 message와 같은 메시지 값들은 그대로 쓰는 것이 아니라 getPayload()로 사용해야 그 값을 전달 가능
+        }
+    }
+
+
+    /** Connection이 종료된 경우 */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        System.out.println("afterConnectionClosed: " + session + " : " + status);
+
+    }
+
+
+}
